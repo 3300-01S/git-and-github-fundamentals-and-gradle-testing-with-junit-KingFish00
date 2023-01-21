7dbec1b760790ea952106d30a536d90bd28fde6b
Grayson <506agrayson@gmail.com>
2023-01-21 11:08:21 -0700

    Removed the new line funtion from inside the string while leaving println alone

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -5,7 +5,8 @@ public class Hello
 	public static void main(final String[] args)
 	{
 		//System.out.println("Not the right string, tests will fail!"); // Old string that deffenetly doesn't work
-		System.out.println("Hello world!\n"); // New string that will hopefully work
+		System.out.println("Hello world!"); // New string that will hopefully work (Take 2), 
+		//I left the new line part of the test code so I was making a hello world with 2 enters intead of just one.
 	}
 
 }
